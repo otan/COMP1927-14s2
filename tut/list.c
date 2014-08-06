f5c276aeb79035edd09942be3cf7018bf22eecdd
Oliver Tan <sinovercosequalstan@gmail.com>
2014-08-06 11:11:12 +1000

    fix

--- tut/list.c
+++ tut/list.c
@@ -2,14 +2,51 @@
 
 typedef struct SortedListRep *SortedList;
 
+typedef struct SortedListNode {
+        int data;
+        Node next;
+} *Node;
+
+struct SortedListRep {
+        Node first;
+        int size;
+};
+
 // create a new empty list
-SortedList newList();
+SortedList newList() {
+        SortedList l = malloc(sizeof(*l));
+        l->first = NULL;
+        l->size = 0;
+        return l;
+}
 
 // insert a value into the list
-void ListInsert(SortedList,int);
+void ListInsert(SortedList l, int num) {
+    Node curr = l->first, prev = NULL;
+    while (curr != NULL && curr->data < num) {
+        prev = curr;
+        curr = curr->next;
+    }
+    Node newN = createNode(num);
+    if (prev != NULL) {
+        prev->next = newN;
+    } else {
+        l->first = newN;
+    }
+    newN->next = curr;
+    l->size++;
+}
 
 // length of list
-int ListLength(SortedList);
+int ListLength(SortedList l) {
+    /*int num = 0;
+    Node curr;
+    for (curr = l->first; curr != NULL; curr = curr->next) {
+        num++;
+    }
+    return num;*/
+    return l->size;
+}
 
 // number of distinct values
 int ListDistinct(SortedList);
